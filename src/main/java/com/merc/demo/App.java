2ab464078e788e5a6673d74adbe40e114f22ed80
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-25 09:43:35 +0530

    spring core

--- src/main/java/com/merc/demo/App.java
+++ src/main/java/com/merc/demo/App.java
@@ -1,13 +1,23 @@
 package com.merc.demo;
 
+import com.merc.demo.models.Employee;
+
 /**
- * Hello world!
+ * 
+ * @author Vaman Deshmukh
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+
+		Employee emp = new Employee(101, "Sonu", 90000d);
+		
+		System.out.println(emp.toString());
+
+		System.out.println("End");
+
+	}
 }
---
+++ src/main/java/com/merc/demo/models/Employee.java
@@ -0,0 +1,49 @@
+package com.merc.demo.models;
+
+public class Employee {
+
+	private Integer eid;
+	private String firstName;
+	private Double salary;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(Integer eid, String firstName, Double salary) {
+		super();
+		this.eid = eid;
+		this.firstName = firstName;
+		this.salary = salary;
+	}
+
+	public Integer getEid() {
+		return eid;
+	}
+
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", firstName=" + firstName + ", salary=" + salary + "]";
+	}
+
+}
