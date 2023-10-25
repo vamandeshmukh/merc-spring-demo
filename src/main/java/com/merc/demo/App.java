621148e7285142502f3644be38b0d8d9240327ea
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-25 10:45:48 +0530

    spring core

--- src/main/java/com/merc/demo/App.java
+++ src/main/java/com/merc/demo/App.java
@@ -1,6 +1,7 @@
 package com.merc.demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.merc.demo.models.Employee;
@@ -24,11 +25,20 @@ public class App {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
 
+		Employee emp0 = new Employee(101, "Sonu", 90000d);
 		Employee emp = context.getBean(Employee.class);
-
 		System.out.println(emp.toString());
+//		Employee emp2 = context.getBean(Employee.class);
+//		System.out.println(emp2.toString());
+//		emp.setFirstName("Sonu");
+//		System.out.println(emp.toString());
+//		System.out.println(emp2.toString());
+//		emp2.setSalary(90000d);
+//		System.out.println(emp.toString());
+//		System.out.println(emp2.toString());
+//		System.out.println("End");
 
-		System.out.println("End");
+		((AbstractApplicationContext) context).close();
 
 	}
 }
@@ -40,11 +50,3 @@ public class App {
 
 //Sample - 
 //https://docs.spring.io/spring-framework/docs/4.2.x/spring-framework-reference/html/xsd-configuration.html
-
-
-
-
-
-
-
-
