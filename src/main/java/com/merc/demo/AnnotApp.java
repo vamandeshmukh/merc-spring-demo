b0501df4a1734930df96cf10eefff896a36ae72a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-25 14:43:26 +0530

    spring core

--- src/main/java/com/merc/demo/AnnotApp.java
+++ src/main/java/com/merc/demo/AnnotApp.java
@@ -11,9 +11,10 @@ import com.merc.demo.models.Employee;
 public class AnnotApp {
 
 	public static void main(String[] args) {
-
+ 
 		ApplicationContext context = new AnnotationConfigApplicationContext(AnnotApp.class);
 		Employee emp = context.getBean(Employee.class);
+		emp.setEid(101);
 		System.out.println(emp.toString());
 
 		((AbstractApplicationContext) context).close();
